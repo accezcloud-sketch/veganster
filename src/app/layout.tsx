2cb4768d5395c6cfe4f038b3f459668d73884ed0
Sufian Kamran <sufikami@gmail.com>
2026-05-13 15:27:29 +0500

    adsense corrected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2065215403709511"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </body>
     </html>
